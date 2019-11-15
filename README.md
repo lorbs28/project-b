ad550611966bcfc598efb5613aea8f8df589a510
Bryan L <lorbs28@gmail.com>
2019-11-14 19:39:48 -0600

    Add README.

---
+++ README.md
@@ -0,0 +1,1 @@
+Project A is a code base that will be used to teach Python, SQL, and anything else.  
--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/health")
+@app.route('/health')
 def health():
     try:
         return 'OKAY!'
     except Exception as e:
         return (str(e))
+
+@app.route('/add/<first>/<second>')
+def add(int(first), int(second)):
+    return first + second
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
